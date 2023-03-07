342fca28b5da9b6a3426acd031739586acd287ac
Wanyea Barbel <wanyea.barbel@knights.ucf.edu>
2023-03-06 21:44:05 -0500

    added sound effects

--- Assignment 2 - Fruit Ninja 3D/Assets/Scripts/EnemyHealth.cs
+++ Assignment 2 - Fruit Ninja 3D/Assets/Scripts/EnemyHealth.cs
@@ -5,20 +5,19 @@ using UnityEngine;
 public class EnemyHealth : MonoBehaviour
 {
     public float enemyHealth = 100.0f;
-
-
+    
     void OnCollisionEnter(Collision collision) 
     {
         if (collision.gameObject.tag == "PlayerWeapon") 
         {
             enemyHealth -= 10.0f;
+            Debug.Log(this.gameObject.name + "health is now " + enemyHealth);
 
             if (enemyHealth == 0 ) 
             {
                 Destroy(this.gameObject);
-                
             }
-            Debug.Log(collision.gameObject.name + "health is now " + enemyHealth);
+
         }
     }
 }
--- Assignment 2 - Fruit Ninja 3D/Assets/Scripts/PlayerHealth.cs
+++ Assignment 2 - Fruit Ninja 3D/Assets/Scripts/PlayerHealth.cs
@@ -17,4 +17,41 @@ public class PlayerHealth : MonoBehaviour
     {
         
     }
+
+    void OnCollisionEnter(Collision collision) 
+    {
+        if (collision.gameObject.name == "Orange Enemy") 
+        {
+            playerHealth -= 20;
+            Debug.Log("Player health is now: " + playerHealth);
+        } 
+
+        if (collision.gameObject.name == "Papaya Enemy") 
+        {
+            playerHealth -= 15;
+            Debug.Log("Player health is now: " + playerHealth);
+
+        }
+
+        if (collision.gameObject.name == "Strawberry Enemy") 
+        {
+            playerHealth -= 5;
+            Debug.Log("Player health is now: " + playerHealth);
+
+        }
+
+        if (collision.gameObject.name == "Dragon Fruit Enemy") 
+        {
+            playerHealth -= 20;
+            Debug.Log("Player health is now: " + playerHealth);
+        }
+
+        if (collision.gameObject.name == "Pomegranate Enemy") 
+        {
+            playerHealth -= 10;
+            Debug.Log("Player health is now: " + playerHealth);
+        }
+
+
+    }
 }

e9549b51cc9aa0fa584d2ab0d3e394e810e165cc
Yousef Abdelsalam <yousefabdelsalam@knights.ucf.edu>
2023-03-06 23:08:14 -0500

    Added weapon swapping

--- Assignment 2 - Fruit Ninja 3D/Assets/Scripts/ControllerHandler.cs
+++ Assignment 2 - Fruit Ninja 3D/Assets/Scripts/ControllerHandler.cs
@@ -6,7 +6,7 @@ public class ControllerHandler : MonoBehaviour
 {
     private OVRPlayerController controller;
     private float moveSpeedMultiplier = 3.0f;
-    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +21,36 @@ public class ControllerHandler : MonoBehaviour
         {
             Debug.Log("A was pressed");
             controller.Jump();
+        }
+
+
+        if (OVRInput.GetDown(OVRInput.Button.Two)) {
+            Debug.Log("B was pressed");
+            GameObject RightHand = GameObject.Find("RightHandAnchor");
+
+            if (RightHand == null) {
+                Debug.Log("RightHand is null?");
+            } else {
+                GameObject sword = RightHand.transform.GetChild(1).gameObject;
+                GameObject shuriken = RightHand.transform.GetChild(2).gameObject;
+
+                // Debug.Log("Sword's name is " + sword.name);
+                // Debug.Log("Shuriken's name is " + shuriken.name);
+
+                // if (!sword.activeSelf) {
+                //     Debug.Log("Sword is currently inactive");
+                // } else if (!shuriken.activeSelf) {
+                //     Debug.Log("Shuriken is currently null");
+                // }
+
+                if (sword.activeSelf) {
+                    sword.SetActive(false);
+                    shuriken.SetActive(true);
+                } else {
+                    shuriken.SetActive(false);
+                    sword.SetActive(true);
+                }
+            }
         }
     }
 }

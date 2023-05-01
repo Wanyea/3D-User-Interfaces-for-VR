579df5092e308e65c167028f0b7b3e2df6f466cb
Wanyea Barbel <wanyea.barbel@knights.ucf.edu>
2023-05-01 15:28:34 -0400

    final update

--- VR Haptic Racing/Assets/Scripts/AIRacerController.cs
+++ VR Haptic Racing/Assets/Scripts/AIRacerController.cs
@@ -16,7 +16,7 @@ public class AIRacerController : MonoBehaviour
     private Rigidbody rb;
     private int currentWaypointIndex = 0;
     [SerializeField] private List<AxleInfo> axleInfos;
-    [SerializeField] private List<Transform> waypoints = new List<Transform>();
+    [SerializeField] private List<Vector3> waypoints;
     public float maxSidewaysFriction = 2.0f;
     public float maxForwardFriction = 1.5f;
 
@@ -38,33 +38,20 @@ public class AIRacerController : MonoBehaviour
             forwardFriction.stiffness = maxForwardFriction;
             axleInfo.leftWheelCollider.forwardFriction = forwardFriction;
             axleInfo.rightWheelCollider.forwardFriction = forwardFriction;
+
+            waypoints.Clear();
+
+            Vector3 initialWaypoint = transform.position + 2.0f * transform.forward;
+            waypoints.Add(initialWaypoint); // Add first point right in front of the AI.
         }
     }
 
     void FixedUpdate()
     {
-
         // Debug.Log(currentWaypointIndex);
 
-        // If there are no waypoints, do nothing
-        if (waypoints.Count == 0)
-        {
-            return;
-        }
-
-        // If we've reached the current waypoint, move to the next one
-        if (Vector3.Distance(transform.position, waypoints[currentWaypointIndex].position) < 1f)
-        {
-            if (currentWaypointIndex == waypoints.Count) 
-            {
-                currentWaypointIndex = 0;
-            } else {
-                currentWaypointIndex++; 
-            }
-        }
-
         // Calculate the direction to the current waypoint
-        Vector3 targetDirection = waypoints[currentWaypointIndex].position - transform.position;
+        Vector3 targetDirection = waypoints[currentWaypointIndex] - transform.position;
         targetDirection.y = 0f;
         targetDirection.Normalize();
 
@@ -73,6 +60,13 @@ public class AIRacerController : MonoBehaviour
 
         // Adjust the steering angle to point towards the current waypoint
         float steering = Mathf.Clamp(angle, -maxSteerAngle, maxSteerAngle);
+
+        // If we've reached the current waypoint, move to the next one
+        if (Vector3.Distance(transform.position, waypoints[currentWaypointIndex]) < 1f)
+        {
+            GenerateNextWayPoint();
+            currentWaypointIndex++;
+        }
 
         // Check the raycasts in front of the car
         RaycastHit hit;
@@ -136,5 +130,46 @@ public class AIRacerController : MonoBehaviour
 
         visualWheel.transform.position = position;
         visualWheel.transform.rotation = rotation;
+    }
+
+    public void GenerateNextWayPoint()
+    {
+
+        float waypointDistance = 2.5f; // Change this to set the distance between waypoints
+
+        Vector3 carPos = transform.position; // Get the position of the car
+
+        // Get the direction the car is facing
+        Vector3 carForward = transform.forward;
+        carForward.y = 0f;
+        carForward.Normalize();
+
+        // Start at the car's position and add waypoints in front of it
+        Vector3 currentPos = carPos;
+
+        // Get a random point in front of the car
+        Vector3 randomOffset = new Vector3(Random.Range(0f, 0.5f), 0f, Random.Range(0.5f, 1f));
+        Vector3 nextPos = currentPos + carForward * waypointDistance + randomOffset;
+
+        // Use a raycast to check if the next waypoint is on the racetrack
+        RaycastHit hit;
+        if (Physics.Raycast(nextPos + Vector3.up * 10f, Vector3.down, out hit, 100f, layerMask))
+        {
+            // Debug.Log("Waypoint on track! Adding it to list...");
+            // Add the waypoint to the list if it's on the racetrack
+            waypoints.Add(hit.point);
+            currentPos = hit.point;
+        } else {
+            // Debug.Log("Waypoint not on track. Steering back towards road...");
+            // The point is not on the racetrack, steer back towards the center of the road
+            Vector3 center = transform.position;
+            center.y = hit.point.y;
+            Vector3 direction = center - hit.point;
+            Vector3 normal = Vector3.Cross(direction, Vector3.up);
+            Vector3 targetPoint = hit.point + normal.normalized * Random.Range(1f, 5f);
+            waypoints.Add(targetPoint);
+            currentPos = targetPoint;
+        }
+     
     }
 }
